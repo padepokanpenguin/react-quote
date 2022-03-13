33019e75bda6dcf7610f9c0ef1eeec4393c419d3
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-13 22:54:53 +0700

    using prompt for preventing possibly unwanted route transition

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,8 +1,13 @@
+import {useHistory} from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuotes = () => {
+  const history = useHistory()
+
   const addQuoteData = (quoteData) => {
     console.log(quoteData)
+
+    history.push('/quotes');
   }
 
   return (
