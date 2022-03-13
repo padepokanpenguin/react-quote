17a47b063a5f4f518e0b8fb286b7ad35640c944b
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-13 22:17:18 +0700

    fixing bug

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,26 @@
 import {useParams, Route} from 'react-router-dom';
 import {Fragment} from 'react';
+import HiglightedQuote from '../components/quotes/HiglightedQuote';
 import Comments from '../components/comments/Comments';
 
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Aria', text: 'Learning is fun' },
+  { id: 'q2', author: 'Jamal', text: 'Practice make perfect' },
+]
+
 const QuoteDetail = () => {
-  const params = useParams()
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if (!quote) {
+    return <p>No quote found</p>
+  }
 
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HiglightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
